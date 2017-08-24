367c3946cd8aa8a1b58f6622788ce4d0e50fa32a
AlexBulgakov <alexbulgakov90@gmail.com>
2017-08-24 18:25:08 +0300

    Add header

---
+++ header.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html class="no-js">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>HelpForComputers</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+    
+    <!-- <!-- CSS -->
+    <link rel="stylesheet" href="<?php bloginfo( "template_directory" ); ?>/css/owl.carousel.css">
+    
+    <link rel="stylesheet" href="<?php bloginfo( "template_directory" ); ?>/css/font-awesome.min.css">
+    <link rel="stylesheet" href="<?php bloginfo( "template_directory" ); ?>/css/main.css">
+    <link rel="stylesheet" href="<?php bloginfo( "template_directory" ); ?>/price/price.css">
+    <link rel="stylesheet" href="<?php bloginfo( "template_directory" ); ?>/css/ionicons.min.css">
+    <link rel="stylesheet" href="<?php bloginfo( "template_directory" ); ?>/css/animate.css">
+    <link rel="stylesheet" href="<?php bloginfo( "template_directory" ); ?>/css/responsive.css">
+    <link rel="stylesheet" href="<?php bloginfo( "template_directory" ); ?>/css/form.css">
+    
+    <!-- Js -->
+
+    <script src="<?php bloginfo( "template_directory" ); ?>/js/vendor/modernizr-2.6.2.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src="<?php bloginfo( "template_directory" ); ?>/js/bootstrap.min.js"></script>
+    <script src="<?php bloginfo( "template_directory" ); ?>/js/owl.carousel.js"></script>
+    <script src="<?php bloginfo( "template_directory" ); ?>/js/plugins.js"></script>
+    <script src="<?php bloginfo( "template_directory" ); ?>/js/min/waypoints.min.js"></script>
+    <script src="<?php bloginfo( "template_directory" ); ?>/js/jquery.counterup.js"></script>
+    <script src="<?php bloginfo( "template_directory" ); ?>/js/jquery.maskedinput.js"></script>
+
+    <!-- Google Map -->
+    <script src="https://maps.googleapis.com/maps/api/js"></script>
+    <script src="<?php bloginfo( "template_directory" ); ?>/js/google-map-init.js"></script>
+
+
+    <script src="<?php bloginfo( "template_directory" ); ?>/js/main.js"></script>
+
+
+  </head>
+
+  <body>
+    <!-- Header Start -->
+  <header>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <!-- header Nav Start -->
+          <nav class="navbar navbar-default">
+            <div class="container-fluid">
+              <!-- Brand and toggle get grouped for better mobile display -->
+              <div class="navbar-header">
+                <a class="navbar-brand" href="index.html">
+                  <img src="<?php bloginfo( "template_directory" ); ?>/img/logo_2_full.png" alt="Logo">
+                </a>
+              </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                  <ul class="nav navbar-nav navbar-center" id="menu">
+                    <li><a href="#about">О нас</a></li>
+                    <li><a href="#guarantee">Гарантия и сроки</a></li>
+                    <li><a href="#repair">Ремонт</a></li>
+                    <li><a href="#reviews">Отзывы</a></li>
+                    <li><a href="#contact">Контакты</a></li>
+                  </ul>
+
+                  <div class="navbar-right">
+                    <a href="tel:+78129937633" style="color: #212121;"><h4>+7 (812) 993-76-33</h4></a>
+                    <p>пн - пт 10<sup>00</sup> - 20<sup>00</sup></p>
+                    <p>сб - вс 12<sup>00</sup> - 20<sup>00</sup></p>
+                  </div>
+                </div><!-- /.navbar-collapse -->
+              </div><!-- /.container-fluid -->
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header><!-- header close -->
